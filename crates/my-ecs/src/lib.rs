079d0d0a9453938b524f8d951b962e254b2a348e
ecoricemon <ecoricemon@gmail.com>
2025-02-13 10:28:12 +0900

    chore: Correct comments on proc macros

--- crates/my-ecs/src/lib.rs
+++ crates/my-ecs/src/lib.rs
@@ -12,12 +12,14 @@ pub(crate) const MAX_GROUP: usize = 4;
 /// Imports all you need at once.
 pub mod prelude {
     pub use super::global;
-    pub use super::my_ecs_macro_with_doc::*;
     pub use super::{default::prelude::*, ds::prelude::*, ecs::prelude::*, util::prelude::*};
     pub use super::{log, tinfo};
+    pub use my_ecs_macros::{filter, request, Component, Entity, Resource};
+    #[doc(no_inline)]
     pub use rayon::prelude::*;
 }
 
+/// A module containing test helper functionalities.
 pub mod test_util {
     pub use super::util::call_timeout;
 }
@@ -29,167 +31,146 @@ pub mod global {
     pub use super::ecs::web::{set_panic_hook_once, web_panic_hook};
 }
 
-/// Imports `my_ecs_macro` with testable doc.
-mod my_ecs_macro_with_doc {
-    /// Implements [`Component`](crate::ecs::ent::component::Component) for the
-    /// type.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use my_ecs::prelude::*;
-    ///
-    /// // Zero-sized marker component.
-    /// #[derive(Component)]
-    /// struct Ca;
-    ///
-    /// #[derive(Component)]
-    /// struct Cb(u8);
-    ///
-    /// #[derive(Component)]
-    /// struct Cc {
-    ///     vel: (f32, f32, f32),
-    ///     acc: (f32, f32, f32),
-    /// }
-    /// ```
-    pub use my_ecs_macros::Component;
+// Reason why we didn't put the doc on re-exports is rust-analyzer doesn't show
+// us the comment in IDEs. rust-analyzer ignores comments on re-exports then
+// only displays comments on source definitions.
+//
+// But the other problem occurs by putting doc comments in the proc macro. The
+// proc macro doesn't know types or traits defined in this outer crate. So
+// corresponding doc tests are here and dealt with as unit tests.
+//
+// This will be removed once rust-analyzer solves the problem above.
+#[cfg(test)]
+mod my_ecs_macros_doc_tests {
+    #![allow(unused)]
+    use crate as my_ecs;
 
-    /// Impelments [`Entity`](crate::ecs::ent::entity::Entity) for the type.
-    ///
-    /// Actually, you don't have to define entity type explicitly, but by doing
-    /// so, the crate becomes to be able to provide easy-to-use functions for
-    /// you.
-    ///
-    /// You can designate which container type you use as well by attributes
-    /// `container` and `random_state`.
-    ///
-    /// `container` means which container type you use for the entity. You can
-    /// use your own types or choose one of built-in types shown below.
-    /// * [`SparseSet`](crate::default::ent_cont::SparseSet) - Default
-    /// * [`ChunkSparseSet`](crate::default::ent_cont::ChunkSparseSet)
-    ///
-    /// `random_state` means a state to make a hasher.
-    /// [`std::hash::RandomState`] is default.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use my_ecs::prelude::*;
-    ///
-    /// #[derive(Component)]
-    /// struct Ca;
-    ///
-    /// #[derive(Entity)]
-    /// struct Ea {
-    ///     a: Ca,
-    /// }
-    ///
-    /// // Or, you can customize entity container.
-    /// #[derive(Entity)]
-    /// #[container(ChunkSparseSet)]
-    /// #[random_state(std::hash::RandomState)]
-    /// struct Eb {
-    ///     a: Ca,
-    /// }
-    /// ```
-    pub use my_ecs_macros::Entity;
+    // SRC DOC: macros/src/lib.rs derive_component
+    #[test]
+    fn test_my_ecs_macros_doc_derive_component() {
+        use my_ecs::prelude::*;
 
-    /// Implements [`Resource`](crate::ecs::resource::Resource) for the type.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use my_ecs::prelude::*;
-    ///
-    /// #[derive(Resource)]
-    /// struct R(i32);
-    /// ```
-    pub use my_ecs_macros::Resource;
+        // Zero-sized marker component.
+        #[derive(Component)]
+        struct Ca;
 
-    /// Implements [`Filter`] for the type, and implements [`Select`] optionally
-    /// if `Target` is defined.
-    ///
-    /// Types implementing `Filter` only can be used in [`EntWrite`] only. Types
-    /// implementing both `Filter` and `Select`, on the other hand, also can be
-    /// used in [`Read`] and [`Write`] as well. Because `Read` and `Write`
-    /// mean requesting read or write access to a specific *target* component.
-    ///
-    /// See [`Filter`] and [`Select`] for more details.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use my_ecs::prelude::*;
-    ///
-    /// #[derive(Component)] struct Ca;
-    /// #[derive(Component)] struct Cb;
-    /// #[derive(Component)] struct Cc;
-    /// #[derive(Component)] struct Cd;
-    /// #[derive(Component)] struct Ce;
-    ///
-    /// // Declares `Fa` with an implemenation of `Filter`.
-    /// filter!(Fa, All = Ca);
-    ///
-    /// // Declares `Fb` with an implemenation of `Filter`.
-    /// filter!(Fb, All = Ca, Any = Cb, None = Cc);
-    ///
-    /// // Declares `Fc` with an implementation of `Filter` and `Select`.
-    /// filter!(Fc, Target = Ca);
-    ///
-    /// // Declares `Fd` with an implementation of `Filter` and `Select`.
-    /// filter!(Fd, Target = Ca, All = Cb, Any = Cc, None = (Cd, Ce));
-    ///
-    /// // All types implement `Filter` which means they can be used in
-    /// // `EntWrite`.
-    /// fn system_a(ew: EntWrite<(Fa, Fb, Fc, Fd)>) { /* ... */ }
-    ///
-    /// // On the other hand, `Fc` and `Fd` can be used in `Read` and `Write`
-    /// // because they implement `Select` too.
-    /// fn system_b(r: Read<Fc>, w: Write<Fd>) { /* ... */ }
-    /// ```
-    ///
-    /// [`Filter`]: crate::ecs::sys::select::Filter
-    /// [`Select`]: crate::ecs::sys::select::Select
-    /// [`EntWrite`]: crate::ecs::sys::query::EntWrite
-    /// [`Read`]: crate::ecs::sys::query::Read
-    /// [`Write`]: crate::ecs::sys::query::Write
-    pub use my_ecs_macros::filter;
+        #[derive(Component)]
+        struct Cb(u8);
 
-    /// Implements [`Request`] for the type.
-    ///
-    /// Functions implement the `Request` by the crate internally, but others
-    /// such as struct or enum don't. You must implement the `Request` yourself
-    /// if you want it to act as a system. This macro helps you write just a
-    /// little bit of code for that.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use my_ecs::prelude::*;
-    ///
-    /// #[derive(Component)] struct Ca;
-    /// #[derive(Component)] struct Cb;
-    /// #[derive(Resource)] struct Ra(i32);
-    /// #[derive(Resource)] struct Rb(i32);
-    ///
-    /// filter!(Fa, Target = Ca);
-    /// filter!(Fb, Target = Cb);
-    /// filter!(Fc, All = (Ca, Cb));
-    ///
-    /// // Declares `Req` with an implementation of `Request`.
-    /// // You can omit Read, Write, ResRead, ResWrite, or EntWrite.
-    /// request!(Req, Read = Fa, Write = Fb, ResRead = Ra, ResWrite = Rb, EntWrite = Fc);
-    ///
-    /// struct Sys {
-    ///     data: String,
-    /// }
-    ///
-    /// impl System for Sys {
-    ///     type Request = Req;
-    ///     fn run(&mut self, resp: Response<'_, Self::Request>) { /* ... */ }
-    /// }
-    /// ```
-    ///
-    /// [`Request`]: crate::ecs::sys::request::Request
-    pub use my_ecs_macros::request;
+        #[derive(Component)]
+        struct Cc {
+            vel: (f32, f32, f32),
+            acc: (f32, f32, f32),
+        }
+    }
+
+    // SRC DOC: macros/src/lib.rs derive_entity
+    #[test]
+    fn test_my_ecs_macros_doc_derive_entity() {
+        use my_ecs::prelude::*;
+
+        #[derive(Component)]
+        struct Ca;
+
+        #[derive(Entity)]
+        struct Ea {
+            a: Ca,
+        }
+
+        // Or, you can customize entity container.
+        #[derive(Entity)]
+        #[container(ChunkSparseSet)]
+        #[random_state(std::hash::RandomState)]
+        struct Eb {
+            a: Ca,
+        }
+    }
+
+    // SRC DOC: macros/src/lib.rs derive_resource
+    #[test]
+    fn test_my_ecs_macros_doc_derive_resource() {
+        use my_ecs::prelude::*;
+
+        #[derive(Resource)]
+        struct R(i32);
+    }
+
+    // SRC DOC: macros/src/lib.rs filter
+    #[test]
+    fn test_my_ecs_macros_doc_filter() {
+        use my_ecs::prelude::*;
+
+        #[derive(Component)]
+        struct Ca;
+        #[derive(Component)]
+        struct Cb;
+        #[derive(Component)]
+        struct Cc;
+        #[derive(Component)]
+        struct Cd;
+        #[derive(Component)]
+        struct Ce;
+
+        // Declares `Fa` with an implemenation of `Filter`.
+        filter!(Fa, All = Ca);
+
+        // Declares `Fb` with an implemenation of `Filter`.
+        filter!(Fb, All = Ca, Any = Cb, None = Cc);
+
+        // Declares `Fc` with an implementation of `Filter` and `Select`.
+        filter!(Fc, Target = Ca);
+
+        // Declares `Fd` with an implementation of `Filter` and `Select`.
+        filter!(Fd, Target = Ca, All = Cb, Any = Cc, None = (Cd, Ce));
+
+        // All types implement `Filter` which means they can be used in
+        // `EntWrite`.
+        fn system_a(ew: EntWrite<(Fa, Fb, Fc, Fd)>) { /* ... */
+        }
+
+        // On the other hand, `Fc` and `Fd` can be used in `Read` and `Write`
+        // because they implement `Select` too.
+        fn system_b(r: Read<Fc>, w: Write<Fd>) { /* ... */
+        }
+    }
+
+    // SRC DOC: macros/src/lib.rs request
+    #[test]
+    fn test_my_ecs_macros_doc_request() {
+        use my_ecs::prelude::*;
+
+        #[derive(Component)]
+        struct Ca;
+        #[derive(Component)]
+        struct Cb;
+        #[derive(Resource)]
+        struct Ra(i32);
+        #[derive(Resource)]
+        struct Rb(i32);
+
+        filter!(Fa, Target = Ca);
+        filter!(Fb, Target = Cb);
+        filter!(Fc, All = (Ca, Cb));
+
+        // Declares `Req` with an implementation of `Request`.
+        // You can omit Read, Write, ResRead, ResWrite, or EntWrite.
+        request!(
+            Req,
+            Read = Fa,
+            Write = Fb,
+            ResRead = Ra,
+            ResWrite = Rb,
+            EntWrite = Fc
+        );
+
+        struct Sys {
+            data: String,
+        }
+
+        impl System for Sys {
+            type Request = Req;
+            fn run(&mut self, resp: Response<'_, Self::Request>) { /* ... */
+            }
+        }
+    }
 }
--- crates/my-ecs/src/macros/src/lib.rs
+++ crates/my-ecs/src/macros/src/lib.rs
@@ -14,6 +14,30 @@ use syn::{
     Result, Token, Type, TypePath, Visibility,
 };
 
+/// Implements [`Component`] for the type.
+///
+/// # Examples
+///
+/// ```ignore
+/// use my_ecs::prelude::*;
+///
+/// // Zero-sized marker component.
+/// #[derive(Component)]
+/// struct Ca;
+///
+/// #[derive(Component)]
+/// struct Cb(u8);
+///
+/// #[derive(Component)]
+/// struct Cc {
+///     vel: (f32, f32, f32),
+///     acc: (f32, f32, f32),
+/// }
+/// ```
+///
+/// [`Component`]: ./trait.Component.html
+//
+// TEST: my-ecs/src/lib.rs: tests::test_my_ecs_macros_doc_derive_component
 #[proc_macro_derive(Component)]
 pub fn derive_component(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
@@ -34,6 +58,49 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Impelments [`Entity`] for the type.
+///
+/// Actually, you don't have to define entity type explicitly, but by doing so,
+/// the crate becomes to be able to provide easy-to-use functions for you.
+///
+/// You can designate which container type you use as well by attributes
+/// `container` and `random_state`.
+///
+/// `container` means which container type you use for the entity. You can use
+/// your own types or choose one of built-in types shown below.
+/// * [`SparseSet`] - Default
+/// * [`ChunkSparseSet`]
+///
+/// `random_state` means a state to make a hasher. [`std::hash::RandomState`] is
+/// default.
+///
+/// # Examples
+///
+/// ```ignore
+/// use my_ecs::prelude::*;
+///
+/// #[derive(Component)]
+/// struct Ca;
+///
+/// #[derive(Entity)]
+/// struct Ea {
+///     a: Ca,
+/// }
+///
+/// // Or, you can customize entity container.
+/// #[derive(Entity)]
+/// #[container(ChunkSparseSet)]
+/// #[random_state(std::hash::RandomState)]
+/// struct Eb {
+///     a: Ca,
+/// }
+/// ```
+///
+/// [`Entity`]: ./trait.Entity.html
+/// [`SparseSet`]: ./struct.SparseSet.html
+/// [`ChunkSparseSet`]: ./struct.ChunkSparseSet.html
+//
+// TEST: my-ecs/src/lib.rs: tests::test_my_ecs_macros_doc_derive_entity
 #[proc_macro_derive(Entity, attributes(container, random_state))]
 pub fn derive_entity(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -309,6 +376,20 @@ pub fn derive_entity(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Implements [`Resource`] for the type.
+///
+/// # Examples
+///
+/// ```ignore
+/// use my_ecs::prelude::*;
+///
+/// #[derive(Resource)]
+/// struct R(i32);
+/// ```
+///
+/// [`Resource`]: ./trait.Resource.html
+//
+// TEST: my-ecs/src/lib.rs: tests::test_my_ecs_macros_doc_derive_resource
 #[proc_macro_derive(Resource)]
 pub fn derive_resource(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
@@ -320,13 +401,55 @@ pub fn derive_resource(input: TokenStream) -> TokenStream {
     })
 }
 
-#[proc_macro]
-pub fn request(input: TokenStream) -> TokenStream {
-    let req = parse_macro_input!(input as Request);
-
-    TokenStream::from(quote! { #req })
-}
-
+/// Implements [`Filter`] for the type, and implements [`Select`] optionally
+/// if `Target` is defined.
+///
+/// Types implementing `Filter` only can be used in [`EntWrite`] only. Types
+/// implementing both `Filter` and `Select`, on the other hand, also can be used
+/// in [`Read`] and [`Write`] as well. Because `Read` and `Write` mean
+/// requesting read or write access to a specific *target* component.
+///
+/// See [`Filter`] and [`Select`] for more details.
+///
+/// # Examples
+///
+/// ```ignore
+/// use my_ecs::prelude::*;
+///
+/// #[derive(Component)] struct Ca;
+/// #[derive(Component)] struct Cb;
+/// #[derive(Component)] struct Cc;
+/// #[derive(Component)] struct Cd;
+/// #[derive(Component)] struct Ce;
+///
+/// // Declares `Fa` with an implemenation of `Filter`.
+/// filter!(Fa, All = Ca);
+///
+/// // Declares `Fb` with an implemenation of `Filter`.
+/// filter!(Fb, All = Ca, Any = Cb, None = Cc);
+///
+/// // Declares `Fc` with an implementation of `Filter` and `Select`.
+/// filter!(Fc, Target = Ca);
+///
+/// // Declares `Fd` with an implementation of `Filter` and `Select`.
+/// filter!(Fd, Target = Ca, All = Cb, Any = Cc, None = (Cd, Ce));
+///
+/// // All types implement `Filter` which means they can be used in
+/// // `EntWrite`.
+/// fn system_a(ew: EntWrite<(Fa, Fb, Fc, Fd)>) { /* ... */ }
+///
+/// // On the other hand, `Fc` and `Fd` can be used in `Read` and `Write`
+/// // because they implement `Select` too.
+/// fn system_b(r: Read<Fc>, w: Write<Fd>) { /* ... */ }
+/// ```
+///
+/// [`Filter`]: ./trait.Filter.html
+/// [`Select`]: ./trait.Select.html
+/// [`EntWrite`]: ./struct.EntWrite.html
+/// [`Read`]: ./struct.Read.html
+/// [`Write`]: ./struct.Write.html
+//
+// TEST: my-ecs/src/lib.rs: tests::test_my_ecs_macros_doc_filter
 #[proc_macro]
 pub fn filter(input: TokenStream) -> TokenStream {
     let sel = parse_macro_input!(input as Select);
@@ -400,6 +523,51 @@ pub fn filter(input: TokenStream) -> TokenStream {
             )*};
         }
     }
+}
+
+/// Implements [`Request`] for the type.
+///
+/// Functions implement the `Request` by the crate internally, but others such
+/// as struct or enum don't. You must implement the `Request` yourself if you
+/// want it to act as a system. This macro helps you write just a little bit of
+/// code for that.
+///
+/// # Examples
+///
+/// ```ignore
+/// use my_ecs::prelude::*;
+///
+/// #[derive(Component)] struct Ca;
+/// #[derive(Component)] struct Cb;
+/// #[derive(Resource)] struct Ra(i32);
+/// #[derive(Resource)] struct Rb(i32);
+///
+/// filter!(Fa, Target = Ca);
+/// filter!(Fb, Target = Cb);
+/// filter!(Fc, All = (Ca, Cb));
+///
+/// // Declares `Req` with an implementation of `Request`.
+/// // You can omit Read, Write, ResRead, ResWrite, or EntWrite.
+/// request!(Req, Read = Fa, Write = Fb, ResRead = Ra, ResWrite = Rb, EntWrite = Fc);
+///
+/// struct Sys {
+///     data: String,
+/// }
+///
+/// impl System for Sys {
+///     type Request = Req;
+///     fn run(&mut self, resp: Response<'_, Self::Request>) { /* ... */ }
+/// }
+/// ```
+///
+/// [`Request`]: ./trait.Request.html
+//
+// TEST: my-ecs/src/lib.rs: tests::test_my_ecs_macros_doc_request
+#[proc_macro]
+pub fn request(input: TokenStream) -> TokenStream {
+    let req = parse_macro_input!(input as Request);
+
+    TokenStream::from(quote! { #req })
 }
 
 #[derive(Debug)]
